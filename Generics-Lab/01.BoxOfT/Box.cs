bd3c16efdcb42cd06171467f709a84057d3ecbe9
kallyy7 <milanova1203@gmail.com>
2018-08-17 15:02:23 +0300

    add

---
+++ Generics-Lab/01.BoxOfT/Box.cs
@@ -0,0 +1,26 @@
+﻿namespace _01.BoxOfT
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Box<T>
+    {
+        private List<T> elements;
+
+        public Box()
+        {
+            this.elements = new List<T>();
+        }
+
+        public void Add(T element) => this.elements.Add(element);
+
+        public T Remove()
+        {
+            var removedElement = this.elements.Last();
+            this.elements.Remove(removedElement);
+            return removedElement;
+        }
+
+        public int Count => this.elements.Count;
+    }
+}
