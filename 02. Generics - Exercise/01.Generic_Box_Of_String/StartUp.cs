4d19372ab37abec66163e5b56d60253f3ef17211
Kalina <milanova1203@gmail.com>
2018-11-30 13:51:01 +0200

    add Generics exercises

---
+++ 02. Generics - Exercise/01.Generic_Box_Of_String/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        int count = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < count; i++)
+        {
+            string strings = Console.ReadLine();
+			
+            Box<string> boxOfStrings = new Box<string>(strings);
+
+            Console.WriteLine(boxOfStrings);
+        }
+    }
+}
+
---
+++ 02. Generics - Exercise/02.Generic_Box_Of_Integer/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        int count = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < count; i++)
+        {
+            int integer = int.Parse(Console.ReadLine());
+			
+            Box<int> boxOfInteger = new Box<int>(integer);
+
+            Console.WriteLine(boxOfInteger);
+        }
+    }
+}
+
